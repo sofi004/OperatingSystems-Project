4878c8356912dba1332b6d547918c13694aefd65
ist1106194 <sophiapiteira@gmail.com>
2023-12-05 14:01:48 +0000

    ee

--- p1_base/main.c
+++ p1_base/main.c
@@ -6,6 +6,7 @@
 #include <unistd.h>
 #include <dirent.h>
 #include <sys/stat.h>
+#include <sys/wait.h>
 #include "constants.h"
 #include "operations.h"
 #include "parser.h"
@@ -15,7 +16,7 @@
 int main(int argc, char *argv[]) {
   unsigned int state_access_delay_ms = STATE_ACCESS_DELAY_MS;
 
-  if (argc > 2) {
+  if (argc > 3) {
     char *endptr;
     unsigned long int delay = strtoul(argv[1], &endptr, 10);
     if (*endptr != '\0' || delay > UINT_MAX) {
@@ -31,8 +32,7 @@ int main(int argc, char *argv[]) {
     return 1;
   }
 
-  if(argc == 2){
-    
+  if(argc == 3){ 
     DIR *directory;
     struct dirent *ent;
     directory = opendir(argv[1]);
@@ -42,31 +42,40 @@ int main(int argc, char *argv[]) {
     char temp_name[256];
     char temp_name_out[256];
     char *trimchar;
+    int process_counter = 0;
+    int status;
     while ((ent = readdir (directory)) != NULL){
-      if(strstr(ent->d_name, ".jobs") != NULL){
-        printf("%s\n", ent->d_name);
-        memset(temp_name, '\0', 256);
-        strcpy(temp_name, job_name);
-        strcat(temp_name, ent->d_name);
-        int fd = open(temp_name, O_RDONLY);
+      int id = fork();
+      process_counter ++;
+      if (id != 0 && process_counter > *argv[2])
+      {
+        wait(&status);
+      }
+      if (id == 0){
+        if(strstr(ent->d_name, ".jobs") != NULL){
+          printf("%s\n", ent->d_name);
+          memset(temp_name, '\0', 256);
+          strcpy(temp_name, job_name);
+          strcat(temp_name, ent->d_name);
+          int fd = open(temp_name, O_RDONLY);
 
-        memset(temp_name_out, '\0', 256);
-        strcpy(temp_name_out, job_name);
-        strcat(temp_name_out, ent->d_name);
-        trimchar = strchr(temp_name_out, '.');
-        strcpy(trimchar, ".out");
-        int fd1 = open(temp_name_out, O_CREAT | O_TRUNC | O_WRONLY, S_IRUSR | S_IWUSR);
+          memset(temp_name_out, '\0', 256);
+          strcpy(temp_name_out, job_name);
+          strcat(temp_name_out, ent->d_name);
+          trimchar = strchr(temp_name_out, '.');
+          strcpy(trimchar, ".out");
+          int fd1 = open(temp_name_out, O_CREAT | O_TRUNC | O_WRONLY, S_IRUSR | S_IWUSR);
 
-        process(fd, fd1);
-        close(fd);
+          process(fd, fd1);
+          close(fd);
+          exit(0);
+        }
       }
     }
     closedir(directory);
     ems_terminate();
     // quando o while acabar quero chamar o ems terminate
-  }
-  
-  
+  }  
   return 0;
-  
 }
+
